6d693b8e161716dc34300440ae78a76f94639490
The Kien DZ <kienthe25@gmail.com>
2024-05-22 23:58:38 +0700

    a

--- src/component/User/ThuTucs/_KhaiBaoTamVang.js
+++ src/component/User/ThuTucs/_KhaiBaoTamVang.js
@@ -262,8 +262,6 @@ export default function _KhaiBaoTamVang() {
                                                     <label className='font-bold text-center  ' > Trụ sở thực hiện </label>
                                                 </div>
                                                 <input
-
-                                                
                                                     className="text-center mb-2 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                     value={` Công an ${form?.coQuanThucHien?.xa} `}
                                                 />
